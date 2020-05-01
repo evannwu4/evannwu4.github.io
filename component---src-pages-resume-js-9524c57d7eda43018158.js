6ab92c71ffae69163f31dea4095a626e4b1f3e26
evannwu4 <evannwu@gmail.com>
2020-05-01 13:35:31 -0400

    Updates

---
+++ component---src-pages-resume-js-9524c57d7eda43018158.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{F2CN:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a);n.default=function(){return o.a.createElement("a",{href:"resume.pdf",download:!0})}}}]);
+//# sourceMappingURL=component---src-pages-resume-js-9524c57d7eda43018158.js.map
--- src/pages/resume.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ResumePage = () => {
-    return (
-        <a href={`resume.pdf`} download />
-    )
-}
-
-export default ResumePage
