289c8cff3ed87098eb777f4f2764741ac7258dbd
evannwu4 <evannwu@gmail.com>
2020-05-04 15:06:51 -0400

    final version

--- src/components/visualization.js
+++ src/components/visualization.js
@@ -32,7 +32,6 @@ class PieClass extends Component {
       .outerRadius(props.outerRadius+160);
     this.colors = [d3.rgb(215, 184, 255),d3.rgb(175, 247, 161),d3.rgb(143, 231, 255)];
     this.valColors = [d3.rgb(164, 112, 230),d3.rgb(100, 204, 80),d3.rgb(0, 152, 194)];
-
     // this.colors = [d3.rgb(66, 215, 245),d3.rgb(242, 204, 128),d3.rgb(205, 146, 247)];
     // this.valColors = [d3.rgb(53, 164, 212),d3.rgb(219, 148, 7),d3.rgb(126, 26, 176)];
     
@@ -41,7 +40,7 @@ class PieClass extends Component {
   componentDidMount() {
     const svg = d3.select(this.ref.current);
     const data = this.createPie1(this.props.data);
-    const { width, height, innerRadius, outerRadius } = this.props;
+    const { width, height, innerRadius, outerRadius, allRawData, week} = this.props;
     var i;
     for (i = 0; i < 7; i++) {
         this.sleepArcs[i] = d3
@@ -87,19 +86,25 @@ class PieClass extends Component {
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc1)
-      .attr("fill", (d, i) => this.colors[0]);
+      .attr("fill", (d, i) => this.colors[0])
+      .on("mouseover", handleMouseOverSleep)
+      .on("mouseout", handleMouseOutSleep);
 
     path
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc2)
-      .attr("fill", (d, i) => this.colors[1]);
+      .attr("fill", (d, i) => this.colors[1])
+      .on("mouseover", handleMouseOverCal)
+      .on("mouseout", handleMouseOutCal);
     
     path
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc3)
-      .attr("fill", (d, i) => this.colors[2]);
+      .attr("fill", (d, i) => this.colors[2])
+      .on("mouseover", handleMouseOverStep)
+      .on("mouseout", handleMouseOutStep);
     path
       .append("path")
       .attr("class", "arc")
@@ -111,34 +116,225 @@ class PieClass extends Component {
       .attr("alignment-baseline", "middle")
       .attr("transform", d => `translate(${this.createArc4.centroid(d)})`)
       .style("fill", "black")
-      .style("font-size", 10)
+      .style("font-size", 14)
+      .style("font-weight", 900)
       .text((d, i) => this.dates[i]);
-    var i;
     for (i = 0; i < 7; i++) {
         path
         .append("path")
-        .attr("class", "arc")
+        .attr("class", "sleep")
         .attr("d", this.sleepArcs[i])
-        .attr("fill", (d, i) => this.valColors[0]);
+        .attr("fill", (d, i) => this.valColors[0])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverSleepData)
+        .on("mouseout", handleMouseOutSleep);
     }
     for (i = 0; i < 7; i++) {
         path
         .append("path")
-        .attr("class", "arc")
+        .attr("class", "cal")
         .attr("d", this.calArcs[i])
-        .attr("fill", (d, i) => this.valColors[1]);
+        .attr("fill", (d, i) => this.valColors[1])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverCalData)
+        .on("mouseout", handleMouseOutCal);
     }
     for (i = 0; i < 7; i++) {
         path
         .append("path")
-        .attr("class", "arc")
+        .attr("class", "step")
         .attr("d", this.stepArcs[i])
-        .attr("fill", (d, i) => this.valColors[2]);
+        .attr("fill", (d, i) => this.valColors[2])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverStepData)
+        .on("mouseout", handleMouseOutStep);
     }
+      
+
+    // Create Event Handlers for mouse
+    function handleMouseOverSleep(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[0][week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../sleepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutSleep(d, i) {
+      console.log("moused out " + i);
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverSleepData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[0][week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../sleepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOverCal(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[1][week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../calorieIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutCal(d, i) {
+      console.log("moused out " + i);
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverCalData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[1][week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../calorieIcon.png")
+      .attr("id", "pic1")
+    }
+     // Create Event Handlers for mouse
+     function handleMouseOverStep(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[2][week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../stepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutStep(d, i) {
+      console.log("moused out " + i);
+
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverStepData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+190} ${outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(allRawData[2][week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${outerRadius+150} ${outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../stepIcon.png")
+      .attr("id", "pic1")
+    }
+
   }
 
   componentWillUpdate(nextProps, nextState) {
-    console.log(nextProps.data);
     const svg = d3.select(this.ref.current);
     const old_group = svg
       .selectAll("g");
@@ -192,46 +388,243 @@ class PieClass extends Component {
       .attr("alignment-baseline", "middle")
       .attr("transform", d => `translate(${this.createArc4.centroid(d)})`)
       .style("fill", "black")
-      .style("font-size", 10)
+      .style("font-size", 14)
+      .style("font-weight", 900)
       .text((d, i) => this.dates[i]);
       
     path
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc1)
-      .attr("fill", (d, i) => this.colors[0]);
+      .attr("fill", (d, i) => this.colors[0])
+      .on("mouseover", handleMouseOverSleep)
+      .on("mouseout", handleMouseOutSleep);
 
     path
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc2)
-      .attr("fill", (d, i) => this.colors[1]);
+      .attr("fill", (d, i) => this.colors[1])
+      .on("mouseover", handleMouseOverCal)
+      .on("mouseout", handleMouseOutCal);
     
     path
       .append("path")
       .attr("class", "arc")
       .attr("d", this.createArc3)
-      .attr("fill", (d, i) => this.colors[2]);
+      .attr("fill", (d, i) => this.colors[2])
+      .on("mouseover", handleMouseOverStep)
+      .on("mouseout", handleMouseOutStep);
+
     for (i = 0; i < 7; i++) {
         path
         .append("path")
         .attr("class", "arc")
         .attr("d", this.sleepArcs[i])
-        .attr("fill", (d, i) => this.valColors[0]);
+        .attr("fill", (d, i) => this.valColors[0])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverSleepData)
+        .on("mouseout", handleMouseOutSleep);
     }
     for (i = 0; i < 7; i++) {
         path
         .append("path")
         .attr("class", "arc")
         .attr("d", this.calArcs[i])
-        .attr("fill", (d, i) => this.valColors[1]);
+        .attr("fill", (d, i) => this.valColors[1])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverCalData)
+        .on("mouseout", handleMouseOutCal);
     }
     for (i = 0; i < 7; i++) {
         path
         .append("path")
         .attr("class", "arc")
         .attr("d", this.stepArcs[i])
-        .attr("fill", (d, i) => this.valColors[2]);
+        .attr("fill", (d, i) => this.valColors[2])
+        .attr("i", i)
+        .on("mouseover", handleMouseOverStepData)
+        .on("mouseout", handleMouseOutStep);
+    }
+    // Create Event Handlers for mouse
+    function handleMouseOverSleep(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[0][nextProps.week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../sleepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutSleep(d, i) {
+      console.log("moused out " + i);
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverSleepData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[0][nextProps.week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../sleepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOverCal(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[1][nextProps.week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../calorieIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutCal(d, i) {
+      console.log("moused out " + i);
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverCalData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[1][nextProps.week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../calorieIcon.png")
+      .attr("id", "pic1")
+    }
+     // Create Event Handlers for mouse
+     function handleMouseOverStep(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      // Specify where to put label of text
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[2][nextProps.week][i]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../stepIcon.png")
+      .attr("id", "pic1")
+    }
+
+    function handleMouseOutStep(d, i) {
+      console.log("moused out " + i);
+
+          // // Select text by id and then remove
+          d3
+          .selectAll("g")
+          .select("#t"+i)
+          .remove();  // Remove text location
+          d3
+          .selectAll("g")
+          .select("#pic1")
+          .remove(); 
+        }
+    function handleMouseOverStepData(d, i) {  // Add interactivity
+      console.log("moused over " + i);
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+190} ${nextProps.outerRadius+160})`)
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("id", "t"+i)
+      .style("fill", "black")
+      .style("font-size", 20)
+      .text(nextProps.allRawData[2][nextProps.week][d3.select(this).attr("i")]);
+
+      svg
+      .append("g")
+      .attr("transform", `translate(${nextProps.outerRadius+150} ${nextProps.outerRadius+60})`)
+      .append("svg:image")
+      .attr('width', 80)
+      .attr('height', 80)
+      .attr("xlink:href", "../../stepIcon.png")
+      .attr("id", "pic1")
     }
   }
 
--- src/pages/dataViz.js
+++ src/pages/dataViz.js
@@ -28,7 +28,38 @@ var allData = [
             [29.86, 16.14, 23.74, 18.33, 20.81, 27.34, 0    ],
         ],
     ]
+var allRawData = [
+    [
+        ["0 hours", "0 hours", "0 hours", "0 hours", "0 hours", "6.06 hours", "7.67 hours"],
+        ["5.65 hours", "7.91 hours", "6.63 hours", "2.72 hours", "6.40 hours", "7.28 hours", "6.18 hours"],
+        ["4.97 hours", "7.83 hours", "8.45 hours", "7.88 hours", "7.75 hours", "6.71 hours", "5.08 hours"],
+        ["4.28 hours", "4.40 hours", "4.93 hours", "7.43 hours", "7.58 hours", "6.16 hours", "7.20 hours"],
+        ["6.43 hours", "7.83 hours", "6.67 hours", "7.72 hours", "6.49 hours", "6.13 hours", "0 hours"]
+    ],
+    [
+        ["0 calories", "0 calories", "0 calories", "0 calories", "0 calories", "2,455 calories", "2,628 calories"],
+        ["3,028 calories", "2,860 calories", "3,178 calories", "2,414 calories", "3,112 calories", "2,983 calories", "2,867 calories"],
+        ["2,919 calories", "3,104 calories", "2,993 calories", "3,215 calories", "3,347 calories", "1,860 calories", "2,812 calories"],
+        ["3,446 calories", "3,147 calories", "3,382 calories", "3,127 calories", "3,857 calories", "2,759 calories", "3,967 calories"],
+        ["3,332 calories", "2,854 calories", "3,152 calories", "2,888 calories", "3,171 calories", "3,685 calories", "0 calories"]
+    ],
+    [
+        ["0 steps", "0 steps", "0 steps", "0 steps", "0 steps", "5,458 steps", "8,848 steps"],
+        ["11,882 steps", "11,099 steps", "12,559 steps", "5,738 steps", "12,960 steps", "8,405 steps", "10,010 steps"],
+        ["10,340 steps", "14,093 steps", "11,732 steps", "13,134 steps", "12,846 steps", "10,262 steps", "7,656 steps"],
+        ["13,008 steps", "11,023 steps", "15,969 steps", "14,121 steps", "18,633 steps", "9,055 steps", "24,461 steps"],
+        ["16,934 steps", "10,070 steps", "13,874 steps", "11,166 steps", "12,409 steps", "15,671 steps", "0 steps"],
 
+    ],
+]
+
+var dateSpecs = [
+    "Feb 1 \u2014 Feb 2",
+    "Feb 3 \u2014 Feb 9",
+    "Feb 10 \u2014 Feb 16",
+    "Feb 17 \u2014 Feb 23",
+    "Feb 24 \u2014 Feb 29"
+]
 
 const DataVizPage = ({ data }) => {
     const [week, setWeek] = useState(0);
@@ -56,15 +87,20 @@ const DataVizPage = ({ data }) => {
                             Fitbit Health Data Dashboard
                         </p>
                         <div className={dataVizStyles.weekButtons}>
-                            <button onClick={prevWeek}>{"<"}{"<"}</button>
-                            <p className={dataVizStyles.weekLabel}>Week {week}</p>
-                            <button onClick={nextWeek}>{">"}{">"}</button>
+                            <button className={dataVizStyles.buttonNext} onClick={prevWeek}>{"<"}{"<"}</button>
+                            <div className={dataVizStyles.dates}>
+                                <p className={dataVizStyles.dateSpecifics}>{dateSpecs[week]}</p>
+                                <p className={dataVizStyles.weekLabel}>Week {week}</p>
+                            </div>
+                            <button className={dataVizStyles.buttonNext} onClick={nextWeek}>{">"}{">"}</button>
                         </div>
                         <PieClass 
                             data={[14.28,14.28,14.28,14.28,14.28,14.28,14.28]} 
                             sleepData={allData[0][week]}
                             calData={allData[1][week]}
                             stepData={allData[2][week]}
+                            allRawData = {allRawData}
+                            week = {week}
                             width = {700}
                             height = {700}
                             innerRadius = {100} 
