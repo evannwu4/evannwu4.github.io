a248b285062d7854ea6f2d4565978fe91d337f36
evannwu4 <evannwu@gmail.com>
2020-05-07 21:47:49 -0400

    styling changes except for links in projects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Layout from '../components/layout'
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-// import Profile from '../resources/img/profile.jpg'
-// import Buggy from '../resources/img/buggy.jpg'
 import indexStyles from '../styles/index.module.scss'
 import '../styles/index.css'
 
@@ -20,7 +18,7 @@ const IndexPage = ({ data }) => {
             <div className={indexStyles.introTextGroup}>
 
                     <h3 className={indexStyles.introText}>
-                        I am a third-year undergraduate student studying electrical and computer engineering at Carnegie Mellon University. I have a strong interest in systems and software engineering and at CMU, I have pursued my interest through courses such as Embedded Systems (18-349), Computer Security (15-330), and Algorithms (15-210). My professional experience thus far has been software engineering internships at General Electric and IBM along with an incoming internship at Microsoft. If you are a recruiter interested in learning more about my work, check out my resume.
+                        I am a third-year undergraduate student studying electrical and computer engineering at Carnegie Mellon University. I have a strong interest in systems and software engineering and at CMU, I have pursued my interest through courses such as Embedded Systems (18-349), Computer Security (15-330), and Algorithms (15-210). My professional experience thus far has been software engineering internships at General Electric and IBM along with an incoming internship at Microsoft. If you are a recruiter interested in learning more about my work, check out my <a href="../../resume.pdf">resume</a>.
                     </h3>
                 </div>
 
@@ -31,7 +29,7 @@ const IndexPage = ({ data }) => {
                 In my free time I enjoy playing tennis, swimming, and basketball. I love the outdoors and some of my favorite activities are hiking, camping, and kayaking. 
             </h3>
             <h3 className={indexStyles.hobbyElement}>
-                I am an avid participant of a CMU sport called buggy. If you don’t know anything about buggy here’s a cool clip of one of our races. I am a veteran mechanic on the team and work primarily on design, manufacturing, and optimization of buggies. 
+                I am an avid participant of a CMU sport called buggy. If you don’t know anything about buggy here’s a cool <a href="https://www.youtube.com/watch?v=j0TpeSgX4u0">clip</a> of one of our races. I am a veteran mechanic on the team and work primarily on design, manufacturing, and optimization of buggies. 
             </h3>
             <Img
                 className={indexStyles.buggyImg}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,57 +1,72 @@
 import React from 'react'
 import Layout from '../components/layout'
-import ProjectItem from '../components/projectItem'
 import projectsStyle from '../styles/projects.module.scss'
-import profile from '../resources/img/github_square_black.png'
-
 
 const ProjectPage = () => {
+    const loadLink = function(link) {
+        window.location.href = link;
+    };
     return (
+        <a href="https://github.com/evannw/htuquantumml">
+
         <Layout>
             <h1>Projects</h1>
-            <ProjectItem>
-                <img src={profile} alt="" className={projectsStyle.projectItemThumbnail} />
+            <div className={projectsStyle.projectItem}>
+                <img src="../../github_square_black.png" alt="" className={projectsStyle.projectItemThumbnail} onClick="function/>
                 <h3 className={projectsStyle.projectItemText}>
-                        Michael Kronovet, Anoop Bhat, Andrew Smith, Evann Wu, and I created the Quantum ML program for Hack The Universe. The premise of the competition was to combine physics and computer science in an interesting and novel way. Our program simulates the evolution of a randomly-generated many body quantum mechanical system. We use a neural network structure that finds local energy minima by gradient descent of a given Hamiltonian (a potential energy function). This program is useful for people looking to study how different Hamiltonian functions affects the evolution of quantum states.
-
+                        Michael Kronovet, Anoop Bhat, Andrew Smith, Tanishq Kancharla, and I created the Quantum ML program for Hack The Universe. The premise of the competition was to combine physics and computer science in an interesting and novel way. Our program simulates the evolution of a randomly-generated many body quantum mechanical system. We use a neural network structure that finds local energy minima by gradient descent of a given Hamiltonian (a potential energy function). This program is useful for people looking to study how different Hamiltonian functions affects the evolution of quantum states.
+                        <br/>
+                        <h3 className={projectsStyle.projectTech}>
                         Core Technologies: Python, NumPy, MatPlotLib
-
+                        </h3>
+                        <h3 className={projectsStyle.projectDate}>
                         Completed Feb. 2019                
-                    </h3>
-            </ProjectItem>
-            <ProjectItem>
-                <img src={profile} alt="" className={projectsStyle.projectItemThumbnail} />
-                <h3 className={projectsStyle.projectItemText}>
-                    Michael Kronovet, Ka Chun Cheung, Evann Wu, and I created the Automated-Logo-Generator for HackCMU. The goal was to create a program that took an image and generated a “logo” version of the main object in the image. We used Adobe Illustrator’s Javascript API to perform the image manipulation and we also used Microsoft Azure to identify the main object as well as associated words/themes to generate a more effective logo. 
-
-                    Core Technologies: Javascript, Microsoft Azure, Adobe Illustrator, AppleScript
-
-                    Completed Sep. 2018            
+                        </h3>
                 </h3>
-            </ProjectItem>
-            <ProjectItem>
-                <img src={profile} alt="" className={projectsStyle.projectItemThumbnail} />
+            </div>
+            <div className={projectsStyle.projectItem}>
+                <img src="../../github_square_black.png" alt="" className={projectsStyle.projectItemThumbnail} />
+                <h3 className={projectsStyle.projectItemText}>
+                    Michael Kronovet, Ka Chun Cheung, Tanishq Kancharla, and I created the Automated-Logo-Generator for HackCMU. The goal was to create a program that took an image and generated a “logo” version of the main object in the image. We used Adobe Illustrator’s Javascript API to perform the image manipulation and we also used Microsoft Azure to identify the main object as well as associated words/themes to generate a more effective logo. 
+                    <br/>
+                    <h3 className={projectsStyle.projectTech}>
+                    Core Technologies: Javascript, Microsoft Azure, Adobe Illustrator, AppleScript
+                    </h3>
+                    <h3 className={projectsStyle.projectDate}>
+                    Completed Sep. 2018            
+                    </h3>
+                </h3>
+            </div>
+            <div className={projectsStyle.projectItem}>
+                <img src="../../github_square_black.png" alt="" className={projectsStyle.projectItemThumbnail} />
                 <h3 className={projectsStyle.projectItemText}>
                     GunAR was a project intending to replicate laser tag in a portable fashion. The experience involved a gun with a phone holster and a interfacing app on the user’s phone. When the trigger on the gun was pressed, it would send a signal to the phone, prompting the phone’s camera to take a picture and analyze if a player had been shot. For a more in-depth explanation, check the Github!
-
+                    <br/>
+                    <h3 className={projectsStyle.projectTech}>
                     Core Technologies: Swift, Raspberry Pi, Javascript
-
-                    Completed Jan. 2018         
+                    </h3>
+                    <h3 className={projectsStyle.projectDate}>
+                    Completed Jan. 2018  
+                    </h3>       
                 </h3>
-            </ProjectItem>
-            <ProjectItem>
-                <img src={profile} alt="" className={projectsStyle.projectItemThumbnail} />
+            </div>
+            <div className={projectsStyle.projectItem}>
+                <img src="../../github_square_black.png" alt="" className={projectsStyle.projectItemThumbnail} />
                 <h3 className={projectsStyle.projectItemText}>
                     Professor Doppleganger is a project that takes a picture of someone’s face as input and finds the CMU professor that looks most similar to them. It uses the Microsoft Azure Face API. The program sends a request to Microsoft Azure with the input image. Then it takes the results and compares it to all CMU professors and generates a score based on similarity. It then returns the image of the professor with the highest score.
-
+                    <br/>
+                    <h3 className={projectsStyle.projectTech}>
                     Core Technologies: Python, Microsoft Azure, Django, Json
-
-                    Completed Feb. 2018       
+                    </h3>
+                    <h3 className={projectsStyle.projectDate}>
+                    Completed Feb. 2018  
+                    </h3>     
                 </h3>
-            </ProjectItem>
+            </div>
 
         </Layout>
     )
 }
+
 
 export default ProjectPage
