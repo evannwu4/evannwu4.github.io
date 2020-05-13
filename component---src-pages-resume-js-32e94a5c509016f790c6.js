d27a30d801dfde02ee9c8864eed20ab2609ec9bd
evannwu4 <evannwu@gmail.com>
2020-05-13 14:50:25 -0400

    Updates

---
+++ component---src-pages-resume-js-32e94a5c509016f790c6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{F2CN:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a);n.default=function(){return o.a.createElement("a",{href:"resume.pdf",download:!0})}}}]);
+//# sourceMappingURL=component---src-pages-resume-js-32e94a5c509016f790c6.js.map
