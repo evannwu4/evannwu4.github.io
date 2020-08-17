b044b3d9b4070efd0a166f2a1a3759990b680147
Evann Wu <evannwu@gmail.com>
2020-08-16 20:48:23 -0400

    added hamburger functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import headerStyles from '../styles/header.module.scss'
+import HamburgerMenu from 'react-hamburger-menu';
+import Menu from '../components/menu'
+
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+    const toggleHamburger = () => {
+        setOpen(!open);
+      };
     return (
         <header className={headerStyles.headerOrg}>
             <div className={headerStyles.iconsList}>
@@ -10,6 +17,19 @@ const Header = () => {
                 <a className={headerStyles.iconAnchor} href="https://www.linkedin.com/in/evannwu/"><img src="../../linkedinIcon.png" alt="" className={headerStyles.icon} /></a>
                 <a className={headerStyles.iconAnchor} href="https://medium.com/@evannwu_15820"><img src="../../mediumIcon.png" alt="" className={headerStyles.icon} /></a>
             </div>
+            <HamburgerMenu
+                className={headerStyles.hamburgerButton}
+                isOpen={open}
+                menuClicked={toggleHamburger}
+                width={18}
+                height={15}
+                strokeWidth={1}
+                rotate={0}
+                color='black'
+                borderRadius={0}
+                animationDuration={0.5}
+            />
+            <Menu open={open}/>
             <div className={headerStyles.textMain}>
                 <h1 className={headerStyles.headerFormat}>
                     <Link className={headerStyles.title} activeClassName={headerStyles.activeTitle} to="/">
