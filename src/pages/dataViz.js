119884cb0f9ddb15eea5370ca04f52557af77e11
Evann Wu <evannwu@gmail.com>
2021-10-17 19:33:19 -0400

    update blog

--- src/pages/dataViz.js
+++ src/pages/dataViz.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Layout from '../components/layout'
 import dataVizStyles from '../styles/dataViz.module.scss'
 import PieClass from '../components/visualization'
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
 
 var allData = [
         [
