aa9e13a7904aa28b00c3185c8aa8ce8be0ab4426
Evann Wu <evannwu@gmail.com>
2022-05-10 11:25:16 -0400

    update resume

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -32,6 +32,9 @@ const Blog = props => {
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
       {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+      <br></br>
+      <br></br>
+      <br></br>
     </Layout>
   )
 }
